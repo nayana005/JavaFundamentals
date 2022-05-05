b0eaff49aaf006fe6fe8b0f30050f036dc374dc6
nayana005 <nayanasuresh1005@gmail.com>
2022-05-05 22:19:48 +0530

    Added main java file

---
+++ Main.java
@@ -0,0 +1,81 @@
+package com.bridgelabz.javafundamentals;
+
+import java.util.Date;
+
+public class Main {
+    byte a = 10;
+    short b = 20;
+    int c = 30;
+    long d = 40;
+    float e = 50.25f;
+    double f = 60.50;
+    boolean g = true;
+    char h = 'a';
+
+    static String i = "Hello";
+    Byte j = 70;
+    Short k = 80;
+    Integer l = 90;
+    Long m = 100l;
+    Float n = 110.10f;
+    Double o = 120.68;
+    Boolean p = false;
+    Character q = 'R';
+
+    Date r = new Date();
+    //Main s = new Main();
+
+    Date[] t; //Array
+
+    static final float PI = 3.14f; //write constants in Capitals
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to JAVA Fundamentals");
+
+        Main m = new Main();
+        int sum = m.add(10,20);
+        System.out.println(sum);
+
+        Parrot parrot1 = new Parrot();
+        parrot1.name = "Green Parrot";
+        parrot1.colour = "Green";
+        parrot1.species = "Bird";
+
+        parrot1.fly();
+        parrot1.swim();
+
+        Parrot parrot3 = new Parrot(10);
+        Parrot parrot4 = new Parrot(true);
+
+        Parrot parrot2 = new Parrot();
+        System.out.println(parrot1.name + " " + parrot2.name);
+        parrot2.name = "White Parrot";
+        parrot2.colour = "White";
+        parrot2.species = "Bird";
+
+        parrot2.fly();
+        parrot2.swim();
+
+        System.out.println(parrot1.name + " " + parrot2.name + " " + Parrot.name);
+
+        Math math = new Math();
+        int x = math.add(10, 20);
+        int y = math.minus(10, 5);
+        int z = math.multiply(5, 2);
+        int c = math.divide(10, 5);
+
+        System.out.println("X = " + x + ", Y = " + y + ", Z = " + z + ", C = " + c);
+
+        System.out.println(m.a);
+    }
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    void display(){
+        Main m2 = new Main();
+        add(10,20);
+        m2.add(20, 40);
+    }
+}
